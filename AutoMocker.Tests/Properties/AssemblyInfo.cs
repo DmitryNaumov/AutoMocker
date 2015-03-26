a8197ed8a09592c0d07aec0845c5d6fc62413072
Dmitry Naumov <dimon-naumov@yandex.ru>
2015-03-27 00:20:15 +0300

    Move tests to separate project

--- AutoMocker.Tests/Properties/AssemblyInfo.cs
+++ AutoMocker.Tests/Properties/AssemblyInfo.cs
@@ -5,25 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("AutoMocker")]
+[assembly: AssemblyTitle("AutoMocker.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoMocker")]
-[assembly: AssemblyCopyright("Copyright © Dmitry Naumov 2014")]
+[assembly: AssemblyProduct("AutoMocker.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("8e8c5d55-42f4-48f8-ab58-64c700a39b6c")]
+[assembly: Guid("98ed900a-215f-48ef-87ae-62a0c1abd1ee")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,8 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("AutoMocker.Tests")]
